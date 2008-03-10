95efde262c5c2bbbfc3560b68cdcb129178e53a0
applehq <applehq@203d044e-caa7-11dc-91ec-67e1038599e7>
2008-03-10 21:03:10 +0000

    Switched around the location of the "Post A Comment"/"No New Comments" and the comment count. It just didn't seem like the right place for them. Now they are :)
    git-svn-id: http://applehq-svn.cvsdude.com/applehq/bb/trunk@13 203d044e-caa7-11dc-91ec-67e1038599e7

--- chatbox.php
+++ chatbox.php
@@ -40,11 +40,8 @@ while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 	if ($line['rateable'] != 1) { $loop .= enclose('div',chrate($line['id']),'class="rate"'); }
   $line['intro'] .= show_pic($line['image']);
 	$loop .= enclose('div',nl2br(html_entity_decode($line['intro'])),'class="text"');
-	if ($line['commentable'] >= 1) {
-		if (comments($line['id']) != 1) { $comment = 's'; } else { $comment = NULL; }
-		$foot .= enclose('a',comments($line['id']). ' comment'.$comment,'href="'.$hurl.'/show/'.$line['id'].'"');
-	}
-	$loop .= enclose('div',$foot,'class="foot"');
+  if ($line['commentable'] == 2) { $loop .= enclose('div',enclose('a','Post a comment','href="'.$hurl.'/show/'.$line['id'].'"'),'class="foot"'); }
+    else { $loop .= enclose('div',enclose('a','No more comments','href="'.$hurl.'/show/'.$line['id'].'"'),'class="foot"'); }
 if (comments($line['id']) >= 1) {
 	$query2 = 'SELECT id,title,date,intro,rateable,rating,image
 		FROM '.$db_prefix.'data
@@ -65,8 +62,9 @@ if (comments($line['id']) >= 1) {
 		if ($line2['rateable'] != 1) { $nloop .= enclose('div',chrate($line2['id']),'class="rate"'); }
     $line2['intro'] .= show_pic($line2['image']);
 		$nloop .= enclose('div',nl2br(html_entity_decode($line2['intro'])),'class="text"');
-    if ($line['commentable'] == 2) { $nloop .= enclose('div',enclose('a','Post a comment','href="'.$hurl.'/show/'.$line['id'].'"'),'class="foot"'); }
-    else { $nloop .= enclose('div',enclose('a','No more comments','href="'.$hurl.'/show/'.$line['id'].'"'),'class="foot"'); }
+    if (comments($line['id']) != 1) { $comment = 's'; } else { $comment = NULL; }
+		$foot .= enclose('a',comments($line['id']). ' comment'.$comment,'href="'.$hurl.'/show/'.$line['id'].'"');
+    $nloop .= enclose('div',$foot,'class="foot"');
 		$comments .= enclose('div',$nloop,'class="entry"');
 	}
 	$loop .= enclose('div',$comments,'id="comments"');
