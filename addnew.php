8f7ccbff54c5f7f38eb06893dbf629f713c4d658
applehq <applehq@203d044e-caa7-11dc-91ec-67e1038599e7>
2008-03-12 13:08:58 +0000

    Fixed bug with pages. Optimised menu Remade cache after new post.

--- addnew.php
+++ addnew.php
@@ -67,11 +67,23 @@ if($allowed == true) {
 } else { exit("There has been an error and you cannot post."); }
 
 if ($commentref == 0) {
-	if ($cat == "chatbox") {
-		header('Location:'.$hurl.'/chatbox');
-	} else { header('Location:'.$hurl.'/'.$cat); }
-
+	$db->fetch('SELECT id,title,date,intro,commentable,rateable,rating,image
+	FROM '.$db_prefix.'data
+	WHERE section = "'.$cat.'"
+		AND moderated != 1
+		AND date <= "'.date($datefmt).'"
+		AND rating >= -50
+	ORDER BY sticky ASC,lastupd DESC, date DESC',0,single_section($cat));
+  header('Location:'.$hurl.'/'.single_section($cat));
 } else { $db->exec('UPDATE '.$db_prefix.'data SET lastupd = "'.date($datefmt).'" WHERE id = "'.$commentref.'" LIMIT 1') or die('Could not update post time (don\'t worry, your post has gone through).');
+$query2 = 'SELECT id,title,date,intro,rateable,rating,commentable,image
+	FROM '.$db_prefix.'data
+	WHERE moderated != 1
+		AND date <= "'.date($datefmt).'"
+		AND commentref="'.$id.'"
+		AND rating >= -50
+	ORDER BY date ASC';
+$result2 = $db->fetch($query2,0,$commentref."com");
 header('Location:'.$hurl.'/show/'.$commentref); }
 
 ?>
