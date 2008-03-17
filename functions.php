ae6173ad8e39fa26a1c11fd5ae8350ed9f58a79b
applehq <applehq@203d044e-caa7-11dc-91ec-67e1038599e7>
2008-03-17 20:32:21 +0000

    Added whitespace text replacement. Removed <a>'s from {{image}}
    git-svn-id: http://applehq-svn.cvsdude.com/applehq/bb/trunk@39 203d044e-caa7-11dc-91ec-67e1038599e7

--- functions.php
+++ functions.php
@@ -7,10 +7,11 @@ require_once('class_db.php');
 $db = new db();
 
 function fixup($text) {
-  $text = preg_replace("%bug #(\d+)%i","<a href=\"http://bugs.theappleman.me.uk/task/$1\">bug #$1</a>",$text);
+  $text = preg_replace("%bug #(\d+)%i","<a href=\"http://bugs.theappleman.me.uk/task/$1\">bug #$1</a>",$text); // bug tracker
   $text = preg_replace("%\[\[(.*?)\|(.*?)\]\]%","<a href=\"$1\">$2</a>",$text); // [[URL|text]] => <a href="URL">text</a>
-  $text = preg_replace("%\{\{(.*?)\|(.*?)\}\}%","<a href=\"$2\"><img src=\"$1\" /></a>",$text);
-  $text = preg_replace("%\{\{(.*?)\}\}%","<a><img src=\"$1\" /></a>",$text);
+  $text = preg_replace("%\{\{(.*?)\|(.*?)\}\}%","<a href=\"$2\"><img src=\"$1\" /></a>",$text); // image with URL
+  $text = preg_replace("%\{\{(.*?)\}\}%","<img src=\"$1\" />",$text); // just image
+  $text = preg_replace("%\s\s+%"," ",$text); // remove whitespace
   $text = preg_replace("%\*(.*?)\*%","<b>*$1*</b>",$text); // *bold* => <b>bold</b>
   //$text = preg_replace("%\s+\/(.*?)\/\s+%","<i>$1</i>",$text); // /italic/ => <i>italic</i>
   //$text = preg_replace("%\s+\_(.*?)\_\s+%","<u>$1</u>",$text); // _underline_ => <u>underline</u>
