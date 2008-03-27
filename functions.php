2f3cbb33944295a3bdccea9ad06c64c19a274d65
applehq <applehq@203d044e-caa7-11dc-91ec-67e1038599e7>
2008-03-27 17:39:07 +0000

    Got pages working, there is 1 known bug.

--- functions.php
+++ functions.php
@@ -199,8 +199,8 @@ function menu() {
 	$return .= enclose('div',$rslt,'class="mainmenu"');
 
   if ( isset($cat) ) {
-    if ( $page === TRUE ) { $pages .= enclose('a','Previous','href="'.$hurl.'/'.$cat.'/'.$page-1.'"'); }
-   $pages .= enclose('a','Previous','href="'.$hurl.'/'.$cat.'/'.$page+1.'"');
+    if ( $page != "" && $page != "0" ) { $pages .= enclose('a','Previous','href="'.$hurl.'/'.$cat.'/p'.($page-1).'"'); }
+   $pages .= enclose('a','Next','href="'.$hurl.'/'.$cat.'/p'.($page+1).'"');
   }
 
   if ($pages) { $return .= enclose('div',$pages,'class=mainmenu'); }
