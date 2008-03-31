45f9f237e9f361b89b03308dd784ace10a198038
applehq <applehq@203d044e-caa7-11dc-91ec-67e1038599e7>
2008-03-31 19:03:55 +0000

    Fixed some bugs. Added edit links to show.

--- show.php
+++ show.php
@@ -7,16 +7,15 @@ require_once('functions.php');
 $id = $_REQUEST['id'];
 $_REQUEST = array(NULL);
 
-$query = 'SELECT title,date,intro,commentable,image
+$query = 'SELECT title,date,intro,commentable,image,ip,useragent
 	FROM '.$db_prefix.'data
 	WHERE id ="' . $id . '"
 	LIMIT 1';
 $result = $db->fetch($query,$cache_time,$id);
-$query2 = 'SELECT title,date,intro,image
+$query2 = 'SELECT id,title,date,intro,image,ip,useragent
 	FROM '.$db_prefix.'data
 	WHERE moderated != 1
 		AND commentref="'.$id.'"
-		AND rating >= -50
 	ORDER BY date ASC';
 $result2 = $db->fetch($query2,$cache_time,$id."com");
 $com_num = 0;
@@ -31,12 +30,14 @@ if ($result) {
   $entry .= enclose('div',get_day($line['date']),'class="bigdate"');
   $entry .= enclose('div',enclose('a',html_entity_decode($line['title']),'href="'.$hurl.'/show/'.$id.'"'),'class="title"');
   $line['intro'] .= show_pic($line['image']);
+  $entry .= enclose('div',$line['date'],'class="date"');
   $entry .= enclose('div',nl2br(fixup(html_entity_decode($line['intro']))),'class="text"');
   if ($line['commentable'] >= 1) {
     if (comments($id) != 1) { $comment = 's'; } else { $comment = NULL; }
     $foot .= enclose('a',comments($id). ' comment'.$comment,'href="'.$hurl.'/show/'.$id.'"');
   }
-  $entry .= enclose('div',$line['date'],'class="foot"');
+  if (levenshtein($line['ip'],$_SERVER['REMOTE_ADDR']) <= 25 && levenshtein($line['useragent'],$_SERVER['HTTP_USER_AGENT']) <= 50) { $edit = enclose('a','edit','href="'.$hurl.'/e/'.$id.'#edit" onclick="return hs.htmlExpand(this, { objectType: \'ajax\'} )"'); } else { $edit = NULL; }
+  $entry .= enclose('div',$edit,'class="foot"');
   $body .= enclose('div',$entry,'class="entry"');
 
   if ($line['commentable'] >= 1) {
@@ -48,10 +49,11 @@ if ($result) {
                 $com_num += 1;
                 $loop .= enclose('div',get_day($line2['date']),'class="bigdate"');
                 $loop .= enclose('div',enclose('a',html_entity_decode($line2['title']),''),'class="title"');
-                $foot .= enclose('div',$line2['date'],'class="date"');
+                $loop .= enclose('div',$line2['date'],'class="date"');
                 $line2['intro'] .= show_pic($line2['image']);
                 $loop .= enclose('div',nl2br(fixup(html_entity_decode($line2['intro']))),'class="text"');
-                $loop .= enclose('div',$foot,'class="foot"');
+		if (levenshtein($line2['ip'],$_SERVER['REMOTE_ADDR']) <= 25 && levenshtein($line2['useragent'],$_SERVER['HTTP_USER_AGENT']) <= 50) { $edit = enclose('a','edit','href="'.$hurl.'/e/'.$line2['id'].'#edit" onclick="return hs.htmlExpand(this, { objectType: \'ajax\'} )"'); } else { $edit = NULL; }
+                $loop .= enclose('div',$edit,'class="foot"');
                 $comments .= enclose('div',$loop,'class="entry"');
 	}
     	}
