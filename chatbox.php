f59e9256ba60aac4e330c01df1ca79d118360a0f
applehq <applehq@203d044e-caa7-11dc-91ec-67e1038599e7>
2008-04-18 15:04:08 +0000

    Moved the chatbox to the top of pages that show it.
    git-svn-id: http://applehq-svn.cvsdude.com/applehq/bb/trunk@89 203d044e-caa7-11dc-91ec-67e1038599e7

--- chatbox.php
+++ chatbox.php
@@ -25,6 +25,12 @@ $head .= head($cat,$id);
 
 $body .= enclose('div',$sitename,'id="head"');
 
+if ($cat == "all") {
+	if(!in_array($default, $nochat)) { $body .= enclose('div',postbox($default,0),'class="entry"'); }
+} else {
+	if(!in_array($cat, $nochat)) { $body .= enclose('div',postbox($cat,0),'class="entry"'); }
+}
+
 if ($result) {
   foreach($result as $line) {
     $loop = NULL; $foot = NULL; $comments = NULL; $rate = NULL;
@@ -48,9 +54,7 @@ if ($result) {
         LIMIT 1';
       $result2 = $db->fetch($query2,$cache_time,$line['id']."1com");
       foreach($result2 as $line2) {
-        $nloop = NULL;
-        $foot = NULL;
-        $rate = NULL;
+        $nloop = NULL;$foot = NULL;$rate = NULL;
         $nloop .= enclose('div',get_day($line2['date']),'class="bigdate"');
         $nloop .= enclose('div',enclose('a',$line2['title'],'href="'.$hurl.'/show/'.$line['id'].'"'),'class="title"');
         $nloop .= enclose('div',$line2['date'],'class="date"');
@@ -65,11 +69,6 @@ if ($result) {
     $body .= enclose('div',$loop,'class="entry"');
   } // foreach
 } // if
-if ($cat == "all") {
-	if(!in_array($default, $nochat)) { $body .= enclose('div',postbox($default,0),'class="entry"'); }
-} else {
-	if(!in_array($cat, $nochat)) { $body .= enclose('div',postbox($cat,0),'class="entry"'); }
-}
 $return = finish_up($head,$body);
 //$return = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' . $return;
 echo $return;
