f0724064c9e148372b9ede4956412e8c4d41b9a3
Daniel Cordero <theappleman@gmail.com>
2009-07-15 15:03:23 +0100

    Username is "Anonymous" by default

--- functions.php
+++ functions.php
@@ -59,7 +59,7 @@ global $hurl, $accept;
     $box .= enclo_s('input','type="hidden" name="transaction_key" value="'.get_transaction_key().'"');
     $box .= enclo_s('input','type="hidden" name="commentable" value="'.$ct.'"');
     $box .= enclo_s('input','type="hidden" name="MAX_FILE_SIZE" value="2097152" ');
-		$box .= enclose('p','Name: '.enclo_s('input','name="title" tabindex="1" accesskey="q"').'&nbsp;'.enclo_s('input','type="file" accesskey="s" name="userfile" tabindex="3"'),'class="name"');
+		$box .= enclose('p','Name: '.enclo_s('input','name="title" tabindex="1" value="Anonymous" accesskey="q"').'&nbsp;'.enclo_s('input','type="file" accesskey="s" name="userfile" tabindex="3"'),'class="name"');
 		$box .= enclose('textarea',$message,'name="intro" rows="5" cols="100" tabindex="2" accesskey="w"');
 		$box .= enclose('div',enclo_s('input','type="submit" tabindex=4" value="Lets go!"')/*.enclo_s('input','type="reset" value="Reset"')*/,'class="foot"');
 		$box = enclose('form',$box,'action="'.$hurl.'/addnew.php" method="post" enctype="multipart/form-data"');
